c78cbe151500426cb199301ef26f78ea21ab2fd9
xuliugen <1573876303@qq.com>
2018-01-21 14:41:16 +0800

    添加favicon.ico文件和部分tag

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 21 2018 13:57:23 GMT+0800 (中国标准时间)
+// build time:Sun Jan 21 2018 14:40:44 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
