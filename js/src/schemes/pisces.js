2bb845931fe04259669ce7500dc271639a1b60a5
xuliugen <1573876303@qq.com>
2018-01-22 10:37:14 +0800

    修改侧边栏显示的二维码的大小和文字间隔以及文字大小

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 21 2018 16:58:19 GMT+0800 (中国标准时间)
+// build time:Mon Jan 22 2018 10:34:53 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
